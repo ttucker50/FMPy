e723298a241beaa6c49c30a328860b7bae815942
Torsten Sommer <torsten.sommer@3ds.com>
2022-02-14 10:41:24 +0100

    Add controller cs test

--- tests/test_fmu_container.py
+++ tests/test_fmu_container.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class FMUContainerBBTest(unittest.TestCase):
 
-    def test_create_fmu_container(self):
+    def test_create_fmu_container_me(self):
 
         resources = os.path.join(os.path.dirname(__file__), 'resources')
 
@@ -74,54 +74,85 @@ class FMUContainerBBTest(unittest.TestCase):
 
         self.assertEqual(problems, [])
 
-        result = simulate_fmu(filename, stop_time=3.5, fmi_call_logger=print)
+        result = simulate_fmu(filename, stop_time=3.5, fmi_call_logger=None)
 
-        # plot_result(result)
+        plot_result(result)
 
-    # def test_create_fmu_container(self):
-    #
-    #     resources = os.path.join(os.path.dirname(__file__), 'resources')
-    #
-    #     configuration = Configuration(
-    #         parallelDoStep=False,
-    #         variables=[
-    #                 Variable(
-    #                     type='Real',
-    #                     variability='tunable',
-    #                     causality='parameter',
-    #                     initial='exact',
-    #                     name='e',
-    #                     start='0.7',
-    #                     description='Coefficient',
-    #                     mapping=[('bouncingBall', 'e')]
-    #                 ),
-    #                 Variable(
-    #                     type='Real',
-    #                     variability='continuous',
-    #                     causality='output',
-    #                     initial='calculated',
-    #                     name='h',
-    #                     description="Height",
-    #                     mapping=[('bouncingBall', 'h')]
-    #                 ),
-    #             ],
-    #         components=[
-    #                 Component(
-    #                     filename=os.path.join(resources, 'BouncingBall.fmu'),
-    #                     interfaceType='ModelExchange',
-    #                     name='bouncingBall'
-    #                 )
-    #             ]
-    #     )
-    #
-    #     filename = 'BouncingBall_container.fmu'
-    #
-    #     create_fmu_container(configuration, filename)
-    #
-    #     problems = validate_fmu(filename)
-    #
-    #     self.assertEqual(problems, [])
-    #
-    #     result = simulate_fmu(filename, stop_time=4, fmi_call_logger=print)
-    #
-    #     plot_result(result)
+    def test_create_fmu_container_cs(self):
+
+        resources = os.path.join(os.path.dirname(__file__), 'resources')
+
+        configuration = Configuration(
+            parallelDoStep=True,
+            description="A controlled drivetrain",
+            variableNamingConvention='structured',
+            unitDefinitions=[
+                Unit(name='rad/s', baseUnit=BaseUnit(rad=1, s=-1), displayUnits=[DisplayUnit(name='rpm', factor=0.1047197551196598)])
+            ],
+            typeDefinitions=[
+                SimpleType(name='AngularVelocity', type='Real', unit='rad/s')
+            ],
+            variables=[
+                Variable(
+                    type='Real',
+                    variability='tunable',
+                    causality='parameter',
+                    initial='exact',
+                    name='k',
+                    start='100',
+                    description='Gain of controller',
+                    mapping=[('controller', 'PI.k')]
+                ),
+                Variable(
+                    type='Real',
+                    variability='continuous',
+                    causality='input',
+                    name='w_ref',
+                    start='0',
+                    description='Reference speed',
+                    mapping=[('controller', 'u_s')],
+                    declaredType='AngularVelocity'
+                ),
+                Variable(
+                    type='Real',
+                    variability='continuous',
+                    causality='output',
+                    initial='calculated',
+                    name='w',
+                    description="Gain of controller",
+                    mapping=[('drivetrain', 'w')],
+                    unit='rad/s',
+                    displayUnit='rpm'
+                ),
+            ],
+            components=[
+                Component(
+                    filename=os.path.join(resources, 'Controller.fmu'),
+                    interfaceType='CoSimulation',
+                    name='controller'
+                ),
+                Component(
+                    filename=os.path.join(resources, 'Drivetrain.fmu'),
+                    interfaceType='CoSimulation',
+                    name='drivetrain',
+                )
+            ],
+            connections=[
+                Connection('drivetrain', 'w', 'controller', 'u_m'),
+                Connection('controller', 'y', 'drivetrain', 'tau'),
+            ]
+        )
+
+        filename = 'ControlledDrivetrain.fmu'
+
+        create_fmu_container(configuration, filename)
+
+        problems = validate_fmu(filename)
+
+        self.assertEqual(problems, [])
+
+        w_ref = np.array([(0.5, 0), (1.5, 1), (2, 1), (3, 0)], dtype=[('time', 'f8'), ('w_ref', 'f8')])
+
+        result = simulate_fmu(filename, start_values={'k': 20}, input=w_ref, output=['w_ref', 'w'], stop_time=4)
+
+        plot_result(result)
